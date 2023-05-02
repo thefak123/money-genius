964543149f7d51e971b5e35d9b456b68ea6c2dfe
ALEXIMTUC21 <113297850+thefak123@users.noreply.github.com>
2023-05-02 11:07:30 +0700

    Initial Commit

---
+++ moneygenius/moneygeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneygeniusApp.swift
+//  moneygenius
+//
+//  Created by MacBook Pro on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct moneygeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
